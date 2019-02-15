eb0abf43952051a91bd82365934e7cdaccd05ece
unknown <sheltmarie11@gmail.com>
2019-02-14 17:16:18 -0800

    added sucking mechanics

--- src/main/java/frc/team5115/robot/statemachines/VacuumStateMachine.java
+++ src/main/java/frc/team5115/robot/statemachines/VacuumStateMachine.java
@@ -3,21 +3,33 @@ package frc.team5115.robot.statemachines;
 import frc.team5115.robot.robot.Robot;
 
 public class VacuumStateMachine extends StateMachineBase {
-    public static final int ON = 1;
-    public static final int INPUT = 2;
+    public static final int INPUT = 3;
+    public static final int BLOWING = 2;
+    public static final int SUCKING = 1;
     public static final int OFF = 0;
     public void update() {
         switch(state) {
             case INPUT:
+                Robot.vacSuc.stop();
                 if(Robot.joy.getSuccOn()) {
-                    state = ON;
-                } else if(Robot.joy.getSuccOff()) {
+                    state = SUCKING;
+                }
+                if(Robot.joy.getSuccOff()) {
                     state = OFF;
                 }
-            case ON:
-                Robot.vacSuc.startSucc();
+                break;
+            case SUCKING:
+                Robot.vacSuc.succ();
+                if(!Robot.joy.getSuccOn()) {
+                    state = INPUT;
+                }
+                break;
             case OFF:
-                Robot.vacSuc.stopSucc();
+                Robot.vacSuc.stop();
+                if(!Robot.joy.getSuccOff()) {
+                    state = INPUT;
+                }
+                break;
         }
     }
 }
--- src/main/java/frc/team5115/robot/subsystems/Vacuum.java
+++ src/main/java/frc/team5115/robot/subsystems/Vacuum.java
@@ -1,19 +1,26 @@
 package frc.team5115.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.Spark;
 
+import javax.naming.ldap.Control;
+
 public class Vacuum {
-    public Spark mrSucc;
+//    public Spark mrSucc;
+    public TalonSRX mrSucc;
 
     public Vacuum() {
-        mrSucc = new Spark(1);
+        mrSucc = new TalonSRX(0);
     }
 
-    public void startSucc() {
-        mrSucc.set(1.0);
+    public void succ() {
+        mrSucc.set(ControlMode.PercentOutput, -.3);
     }
 
-    public void stopSucc() {
-        mrSucc.set(0);
+    public void stop() {
+        mrSucc.set(ControlMode.PercentOutput, 0);
     }
+
+    public void blow() {mrSucc.set(ControlMode.PercentOutput, .3);}
 }
